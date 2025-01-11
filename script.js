34d2251970619f11b699ad5695a6c0b7cfc6859b
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-11 20:34:03 +0100

    feat add event

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-//DOM ELM
+// DOM ELEMENTS
 const cardContainer = document.getElementById('card');
+const closeBtn = document.getElementById('close-btn');
+const overlayElm = document.getElementById('overlay');
+const overlayImgElm = document.getElementById('overlay-img');
 
-//FUNCTION
+// FUNCTION
 function getCard() {
     axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         .then(function (response) {
@@ -11,19 +14,37 @@ function getCard() {
                 const title = card.title;
                 const img = card.url;
 
-            cardContainer.innerHTML += `
-             <div class="col-lg-4 col-sm-6 col-12 p-2">
-                <div class="card col bg-light p-3 mt-4 position-relative">
-                <img class="pin" src="img/pin.svg" alt="pin">
-                    <img src="${img}" alt="immagine API" class="img-fluid">
-                    <p class="mt-2">${title}</p>
-                </div>
-             </div>
-            `;
+                cardContainer.innerHTML += `
+                    <div class="col-lg-4 col-sm-6 col-12 p-2">
+                        <div class="card bg-light p-3 mt-4 position-relative img-card">
+                            <img class="pin" src="img/pin.svg" alt="pin">
+                            <img src="${img}" alt="${title}" class="img-fluid">
+                            <p class="mt-2">${title}</p>
+                        </div>
+                    </div>
+                `;
+            });
+
+            const photoCards = document.querySelectorAll('.img-card');
+            photoCards.forEach((photoCard) => {
+                photoCard.addEventListener("click", () => {
+                    const imgCardElm = photoCard.querySelector('.img-fluid');
+                    const imgSrcElm = imgCardElm.src; 
+                    overlayImgElm.src = imgSrcElm; 
+                    overlayElm.classList.remove("hidden"); 
+                });
+            });
+        })
+        .catch(function (error) {
+            console.error('Error fetching cards:', error);
         });
-    })
 }
 
-getCard()
+const closeOverlay = () => {
+    overlayElm.classList.add("hidden");
+};
 
-//EVENTS
+getCard();
+
+// EVENT
+closeBtn.addEventListener('click', closeOverlay);
