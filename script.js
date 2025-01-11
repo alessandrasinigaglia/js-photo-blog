481cc35e9ee86a106eaa91fc1fcf9d0181f390a3
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-11 20:35:46 +0100

    feat: adjust

--- script.js
+++ script.js
@@ -35,9 +35,6 @@ function getCard() {
                 });
             });
         })
-        .catch(function (error) {
-            console.error('Error fetching cards:', error);
-        });
 }
 
 const closeOverlay = () => {
